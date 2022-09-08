4e8a2a9a5f5f0aad39c981df3ddfceb8615f276c
xPainite <58267901+happy-ruby@users.noreply.github.com>
2022-09-08 08:59:33 -0400

    Message handler
    Create game and save a game properly

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,8 +12,34 @@ import (
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// // TODO: Handling the message
+	// _ = ctx
 
-	return &types.MsgCreateGameResponse{}, nil
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
+
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Game:  newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Red:   msg.Red,
+		Black: msg.Black,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
